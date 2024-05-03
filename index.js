aeb9d5712398d48650c6e155649d80ab0c6f51e8
josue_delgado <josuedelgado251@gmail.com>
2024-05-02 22:48:57 -0300

    2/5/2024

---
+++ index.js
@@ -0,0 +1,44 @@
+//var nombre = "josue"    
+//document.writeln(prompt("todo bien?"))
+document.write("<h1>hola panas</h1>")
+console.log("estoy probando la consola");
+/*incremento con ++ o --
+var sueldo = 80;
+++sueldo
+alert(sueldo)*/
+
+/*var salarioLunes = 100
+var salariomartes = 200
+var total = salarioLunes * salariomartes
+alert(total)*/ 
+
+/*var valorA = true;
+var valorB = false;
+
+var resultado = valorA && valorB
+alert(resultado)*/
+
+/*var valorA = false;
+var valorB = false;
+
+var resultado = valorA || valorB
+alert(resultado)*/
+
+/* ventana + variable
+var faso = window.prompt("queres drogaWcompa?", 'si queres decime aca gato');
+
+alert("el amigo quiere: " + faso)*/
+/*
+var confirmacion = window.confirm("aceptas o no gaturro??")
+
+alert(confirmacion)*/
+
+/*condicional if / else - else if
+var edad = prompt('ingrese su edad')
+if(edad >= 18){
+    console.log('usted es mayor de edad')
+} else if(edad > 14 && edad < 18) {
+    console.log("usted es  adolescente")
+} else {
+    console.log('sos un pendejo')
+}*/
